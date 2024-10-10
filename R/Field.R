cd80b908cb41e52057d7e2a6a44ac0417078e5d3
Luke Zappia <luke@data-intuitive.com>
2024-10-10 15:44:41 +0200

    Add printing to Field

--- R/Field.R
+++ R/Field.R
@@ -45,6 +45,30 @@ Field <- R6::R6Class( # nolint object_name_linter
       private$.related_field_name <- related_field_name
       private$.related_registry_name <- related_registry_name
       private$.related_module_name <- related_module_name
+    },
+    print = function(style = TRUE) {
+      cli::cat_line(self$to_string(style))
+    },
+    to_string = function(style = FALSE) {
+
+      field_strings <- make_key_value_strings(
+        self,
+        c(
+          "field_name",
+          "column_name",
+          "type",
+          "registry_name",
+          "module_name",
+          "through",
+          "is_link_table",
+          "relation_type",
+          "related_field_name",
+          "related_registry_name",
+          "related_module_name"
+        )
+      )
+
+      make_class_string("Field", field_strings, style = style)
     }
   ),
   private = list(
