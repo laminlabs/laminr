1abb9f4a769f3eac6c8ca2794c4c7bba82964226
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-01 12:31:15 +0100

    add test for to_string

--- tests/testthat/test-Registry.R
+++ tests/testthat/test-Registry.R
@@ -10,3 +10,22 @@ test_that("df works", {
   expect_s3_class(records, "data.frame")
   expect_true(all(c("description", "created_at", "id", "uid") %in% colnames(records)))
 })
+
+test_that("to_string works", {
+  local_setup_lamindata_instance()
+
+  db <- connect("laminlabs/lamindata")
+
+  str <- db$bionty$Phenotype$to_string()
+  # example:
+  # nolint start line_length_linter
+  # Phenotype(SimpleFields=[id, uid, abbr, name, synonyms, created_at, updated_at, description, ontology_id], RelationalFields=[run, artifacts, created_by], BiontyFields=[source, parents, children])
+  # nolint end line_length_linter
+
+  expect_type(str, "character")
+
+  expect_match(str, "Phenotype\\([^\\)]+\\)")
+  expect_match(str, "SimpleFields=\\[id, uid,[^\\]*\\]")
+  expect_match(str, "RelationalFields=\\[[^\\]*\\]")
+  expect_match(str, "BiontyFields=\\[[^\\]*\\]")
+})
