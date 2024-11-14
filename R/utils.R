d0bfe5a7cee50272675d13c38b1c80878216aaf2
Luke Zappia <luke@data-intuitive.com>
2024-11-14 12:34:47 +0100

    Adjust check_requires to output warnings

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,40 @@
 #' Check required packages
 #'
-#' Check that required packages are available and give a nice error message with
+#' Check that required packages are available and give a nice message with
 #' install instructions if not
 #'
 #' @param what A message stating what the packages are required for. Used at the
 #'   start of the error message e.g. "{what} requires...".
 #' @param requires Character vector of required package names
+#' @param type Type of message to give if packages are missing
 #'
-#' @return `TRUE` invisibly if all packages are available, otherwise calls
-#'   [cli::cli_abort()]
+#' @return Invisibly, Boolean whether or not all packages are available or
+#'   raises an error if any are missing and `type = "error"`
 #' @noRd
-check_requires <- function(what, requires) {
+check_requires <- function(what, requires, type = c("error", "warning")) {
+  type <- match.arg(type)
+
   is_available <- map_lgl(requires, requireNamespace, quietly = TRUE)
+
+  msg_fun <- switch (type,
+    error = cli::cli_abort,
+    warning = cli::cli_warn
+  )
 
   if (any(!is_available)) {
     missing <- requires[!is_available]
     missing_str <- paste0("'", paste(missing, collapse = "', '"), "'") # nolint object_usage_linter
-    cli_abort(
+    msg_fun(
       c(
         "{what} requires the {.pkg {missing}} package{?s}",
         "i" = paste(
-          "To continue, install {cli::qty(missing)}{?it/them} using",
-          "{.code install.packages(c({missing_str}))}"
+          "Install {cli::qty(missing)}{?it/them} using",
+          "{.run install.packages(c({missing_str}))}"
         )
       ),
       call = rlang::caller_env()
     )
   }
 
-  invisible(TRUE)
+  invisible(all(is_available))
 }
