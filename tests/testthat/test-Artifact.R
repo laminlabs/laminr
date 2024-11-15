62540e692fb8f648f55ab245567e733e7f31e63b
Luke Zappia <luke@data-intuitive.com>
2024-11-15 13:23:29 +0100

    Add test for Artifact$from_df()

---
+++ tests/testthat/test-Artifact.R
@@ -0,0 +1,21 @@
+skip_if_offline()
+
+test_that("creating an artifact from a data frame works", {
+  skip_if_not_installed("reticulate")
+  skip_if_not(reticulate::py_available("lamindb"))
+
+  local_setup_lamindata_instance()
+
+  db <- connect()
+
+  dataframe <- data.frame(
+    Description = "laminr test data frame",
+    Timestamp = Sys.time()
+  )
+
+  new_artifact <- db$Artifact$from_df(
+    dataframe, description = dataframe$Description
+  )
+
+  expect_s3_class(new_artifact, "TemporaryArtifact")
+})
