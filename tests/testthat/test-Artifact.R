72a0a51b5bcfd5adba8f7a0145f524c0649a146d
Luke Zappia <luke@data-intuitive.com>
2024-11-19 11:26:30 +0100

    Add test for Registry$from_anndata()

--- tests/testthat/test-Artifact.R
+++ tests/testthat/test-Artifact.R
@@ -21,7 +21,7 @@ test_that("creating an artifact from a data frame works", {
   expect_s3_class(new_artifact, "TemporaryArtifact")
 })
 
-test_that("creating an artifacts from a file works", {
+test_that("creating an artifact from a file works", {
   skip_if_not_installed("reticulate")
   skip_if_not(reticulate::py_module_available("lamindb"))
 
@@ -63,4 +63,27 @@ test_that("creating artifacts from a directory works", {
   expect_length(new_records, 2)
   expect_s3_class(new_records[[1]], "TemporaryArtifact")
   expect_s3_class(new_records[[2]], "TemporaryArtifact")
+})
+
+test_that("creating an artifact from an AnnData works", {
+  skip_if_not_installed("reticulate")
+  skip_if_not(reticulate::py_module_available("lamindb"))
+  skip_if_not_installed("anndata")
+
+  local_setup_lamindata_instance()
+
+  db <- connect()
+
+  adata <- anndata::AnnData(
+    X = matrix(rnorm(10 * 20), nrow = 10, ncol = 20),
+    obs = data.frame(Letter = LETTERS[1:10]),
+    uns = list(Description = "laminr test AnnData")
+  )
+
+  new_artifact <- db$Artifact$from_df(
+    adata,
+    description = adata$uns$Description
+  )
+
+  expect_s3_class(new_artifact, "TemporaryArtifact")
 })
