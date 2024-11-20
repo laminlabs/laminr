f31731f2ac207aaf0dab1694677f8e1d4d63bf81
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-20 17:36:52 +0100

    update tibble imports

--- R/laminr-package.R
+++ R/laminr-package.R
@@ -7,5 +7,6 @@
 #' @importFrom purrr map map_chr map_lgl map2 pmap list_flatten transpose discard keep list_c walk reduce
 #' @importFrom R6 R6Class
 #' @importFrom rlang set_names
+#' @importFrom tibble tibble as_tibble
 ## usethis namespace: end
 NULL
