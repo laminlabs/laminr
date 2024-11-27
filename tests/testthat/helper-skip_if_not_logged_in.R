ce3f7c50f2da0aa6b57cd581efc42136f6ffaef2
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-27 17:16:29 +0100

    fix missing import

--- tests/testthat/helper-skip_if_not_logged_in.R
+++ tests/testthat/helper-skip_if_not_logged_in.R
@@ -1,4 +1,4 @@
 skip_if_not_logged_in <- function() {
   user_settings <- .get_user_settings()
-  skip_if(is.null(user_settings$access_token), "You must be logged in to run this test")
+  testthat::skip_if(is.null(user_settings$access_token), "You must be logged in to run this test")
 }
