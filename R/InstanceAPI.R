769e3a208693abbb434810ff2c092c25e88f82b1
Luke Zappia <luke@data-intuitive.com>
2024-11-28 10:36:37 +0100

    Add private InstanceAPI method to get headers

--- R/InstanceAPI.R
+++ R/InstanceAPI.R
@@ -31,18 +31,9 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
         "/schema"
       )
 
-      headers <- c(
-        accept = "application/json",
-        `Content-Type` = "application/json"
-      )
-      user_settings <- .get_user_settings()
-      if (!is.null(user_settings$access_token)) {
-        headers[["Authorization"]] <- paste("Bearer", user_settings$access_token)
-      }
-
       response <- httr::GET(
         url,
-        httr::add_headers(.headers = headers)
+        httr::add_headers(.headers = private$get_headers())
       )
 
       private$process_response(response, "get schema")
@@ -104,15 +95,6 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
         tolower(include_foreign_keys)
       )
 
-      headers <- c(
-        accept = "application/json",
-        `Content-Type` = "application/json"
-      )
-      user_settings <- .get_user_settings()
-      if (!is.null(user_settings$access_token)) {
-        headers[["Authorization"]] <- paste("Bearer", user_settings$access_token)
-      }
-
       if (verbose) {
         cli_inform("URL: {url}")
         cli_inform("Body: {jsonlite::minify(body)}")
@@ -120,7 +102,7 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
 
       response <- httr::POST(
         url,
-        httr::add_headers(.headers = headers),
+        httr::add_headers(.headers = private$get_headers()),
         body = body
       )
 
@@ -198,15 +180,6 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
         tolower(include_foreign_keys)
       )
 
-      headers <- c(
-        accept = "application/json",
-        `Content-Type` = "application/json"
-      )
-      user_settings <- .get_user_settings()
-      if (!is.null(user_settings$access_token)) {
-        headers[["Authorization"]] <- paste("Bearer", user_settings$access_token)
-      }
-
       if (verbose) {
         cli_inform("URL: {url}")
         cli_inform("Body: {jsonlite::minify(body)}")
@@ -214,7 +187,7 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
 
       response <- httr::POST(
         url,
-        httr::add_headers(.headers = headers),
+        httr::add_headers(.headers = private$get_headers()),
         body = body
       )
 
@@ -226,13 +199,6 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
                              registry_name,
                              id_or_uid,
                              verbose = FALSE) {
-      user_settings <- .get_user_settings()
-      if (is.null(user_settings$access_token)) {
-        cli::cli_abort(c(
-          "There is no access token for the current user",
-          "i" = "Run {.code lamin login} and reconnect to the database in a new R session"
-        ))
-      }
 
       url <- paste0(
         private$.instance_settings$api_url,
@@ -255,9 +221,7 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
       response <- httr::DELETE(
         url,
         httr::add_headers(
-          accept = "application/json",
-          `Content-Type` = "application/json",
-          Authorization = paste("Bearer", user_settings$access_token)
+          .headers = private$get_headers(authorization_required = TRUE)
         )
       )
 
@@ -286,6 +250,24 @@ InstanceAPI <- R6::R6Class( # nolint object_name_linter
   ),
   private = list(
     .instance_settings = NULL,
+    get_headers = function(authorization_required = FALSE) {
+      headers <- c(
+        accept = "application/json",
+        `Content-Type` = "application/json"
+      )
+      user_settings <- .get_user_settings()
+
+      if (!is.null(user_settings$access_token)) {
+        headers[["Authorization"]] <- paste("Bearer", user_settings$access_token)
+      } else if (authorization_required) {
+        cli::cli_abort(c(
+          "There is no access token for the current user",
+          "i" = "Run {.code lamin login} and reconnect to the database in a new R session"
+        ))
+      }
+
+      return(headers)
+    },
     process_response = function(response, request_type) {
       content <- httr::content(response)
       if (httr::http_error(response)) {
