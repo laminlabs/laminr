9467aab20f0704b372a4199bf69c7647486b9969
Luke Zappia <luke@data-intuitive.com>
2025-02-20 16:30:13 +0100

    ✅ Add tests for install_lamindb()

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,17 @@
-# Run before any tests
+# Run before any tests ---------------------------------------------------------
+# Get a unique name for this test run
 timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
 instance_name <- paste0("laminr-test-", timestamp)
 temp_storage <- file.path(tempdir(), instance_name)
+# Create a test instance
 lamin_init(temp_storage, name = instance_name, modules = "bionty")
+# Connect to the test instance
 lamin_connect(instance_name)
+# Import lamindb so we don't have to do it in every test
 ln <- import_lamindb()
 
-# Run after all tests
+# Run after all tests ----------------------------------------------------------
+# Delete the test instance
 withr::defer(lamin_delete(instance_name, force = TRUE), testthat::teardown_env())
+# Reset the default instance so we can connect to another
+withr::defer(options(LAMINR_DEFAULT_INSTANCE = NULL), testthat::teardown_env())
---
+++ tests/testthat/test-install.R
@@ -0,0 +1,15 @@
+# Remove the test environment when we are done
+withr::defer(
+  reticulate::virtualenv_remove(instance_name, confirm = FALSE)
+)
+
+test_that("install_lamindb() works", {
+  expect_no_error(install_lamindb(envname = instance_name, use = FALSE))
+})
+
+test_that("install_lamindb() works with extra packages", {
+  expect_no_error(
+    install_lamindb(
+      envname = instance_name, extra_packages = c("bionty", "wetlab"), use = FALSE)
+  )
+})
