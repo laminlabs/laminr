a47245d8028b6f1d69195be2b263bff23b9239c3
Luke Zappia <luke@data-intuitive.com>
2025-02-21 09:50:05 +0100

    ✅ Add Registry tests

---
+++ tests/testthat/test-Registry.R
@@ -0,0 +1,29 @@
+test_that("Wrapping a Registry works", {
+  expect_s3_class(ln$ULabel, "laminr.lamindb.models.Registry")
+})
+
+test_that("Calling a Registry works", {
+  label <- expect_no_error(ln$ULabel(name = "My label"))
+
+  expect_s3_class(label, "lamindb.models.ULabel")
+})
+
+test_that("Registry$from_df() works", {
+  df <- data.frame(
+    Letters = LETTERS[1:5],
+    Numbers = 1:5
+  )
+  artifact <- expect_no_error(
+    ln$Artifact$from_df(df, description = "My data frame")
+  )
+  expect_s3_class(artifact, "laminr.lamindb.models.Artifact")
+
+  df <- data.frame(
+    Letters = letters[1:5],
+    Numbers = 1:5
+  )
+  artifact <- expect_no_error(
+    ln$Artifact$from_df(df, revises = artifact)
+  )
+  expect_s3_class(artifact, "laminr.lamindb.models.Artifact")
+})
