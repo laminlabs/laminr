b42cb53fdd88b040b1903ae70a7b1e4d70bb6b3c
Luke Zappia <luke@data-intuitive.com>
2025-03-05 15:07:17 +0100

    🧑‍💻 Create test environment in test setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,37 @@
+# Get a name for this test run
+timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
+test_name <- paste0("laminr-test-", timestamp)
+
+# Create a new test environment
+install_lamindb(envname = test_name, extra_packages = "bionty")
+if (Sys.getenv("LAMIN_TEST_VERSION") == "devel") {
+  reticulate::py_install("git+https://github.com/laminlabs/lamindb.git")
+}
+reticulate::py_config()
+withr::defer(
+  {
+    tryCatch(
+      reticulate::virtualenv_remove(test_name, confirm = FALSE),
+      error = function(err) {
+        reticulate::conda_remove(test_name)
+      }
+    )
+  },
+  testthat::teardown_env()
+)
+
 # Reset the current instance when we are done
 current_instance <- laminr::get_current_lamin_instance()
 withr::defer(lamin_connect(current_instance), testthat::teardown_env())
 
 # Create a temporary test instance
-lamin_init_temp("laminr-test", modules = "bionty", envir = testthat::teardown_env())
+lamin_init_temp(
+  test_name,
+  modules = "bionty",
+  add_timestamp = FALSE,
+  envir = testthat::teardown_env()
+)
+
 # Import lamindb so we don't have to do it in every test
 ln <- import_lamindb()
 # Reset the default instance so we can connect to another
--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -1,17 +1,22 @@
 # Remove the test environment when we are done
-test_env <- strsplit(get_current_lamin_instance(), "/")[[1]][2]
+test_install_env <- paste0(
+  strsplit(get_current_lamin_instance(), "/")[[1]][2],
+  "-install"
+)
 withr::defer(
-  reticulate::virtualenv_remove(test_env, confirm = FALSE)
+  reticulate::virtualenv_remove(test_install_env, confirm = FALSE)
 )
 
 test_that("install_lamindb() works", {
-  expect_no_error(install_lamindb(envname = test_env, use = FALSE))
+  expect_no_error(install_lamindb(envname = test_install_env, use = FALSE))
 })
 
 test_that("install_lamindb() works with extra packages", {
   expect_no_error(
     install_lamindb(
-      envname = test_env, extra_packages = c("bionty", "wetlab"), use = FALSE
+      envname = test_install_env,
+      extra_packages = c("bionty", "wetlab"),
+      use = FALSE
     )
   )
 })
