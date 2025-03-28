bd2a1fbad2b80a736f980a45b00b86c974a4f70b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-03-28 10:11:11 +0100

    ✏️ Fix typos

--- test-docs/r-quickstart.R
+++ test-docs/r-quickstart.R
@@ -1,13 +1,13 @@
 library(laminr)
 ln <- import_module("lamindb")  # instantiate the central object of the API
 
-# Access inputs
+# Access inputs -------------------------------------------
 
 ln$track()  # track your run of a notebook or script
-artifact <- ln$Artifact$using("laminlabs/cellxgene")$get("7dVluLROpalzEh8m")  # https://lamin.ai/laminlabs/cellxgene/artifact/7dVluLROpalzEh8m
+artifact <- ln$Artifact$using("laminlabs/cellxgene")$get("7dVluLROpalzEh8m")  # query the artifact https://lamin.ai/laminlabs/cellxgene/artifact/7dVluLROpalzEh8m
 adata <- artifact$load()  # load the artifact into memory or sync to cache via filepath <- artifact$cache()
 
-# Your transformation
+# Your transformation -------------------------------------
 
 library(Seurat)  # find marker genes with Seurat
 seurat_obj <- CreateSeuratObject(counts = as(Matrix::t(adata$X), "CsparseMatrix"), meta.data = adata$obs)
@@ -18,8 +18,8 @@ markers <- FindAllMarkers(seurat_obj, features = Features(seurat_obj)[1:100])
 seurat_path <- tempfile(fileext = ".rds")
 saveRDS(seurat_obj, seurat_path)
 
-# Save outputs
+# Save outputs --------------------------------------------
 
-ln$Artifact(seurat_path, key = "my-datasets/my-seurat-object.rds")$save()
-ln$Artifact$from_df(markers, key = "my-datasets/my-markers.parquet")$save()
+ln$Artifact(seurat_path, key = "my-datasets/my-seurat-object.rds")$save()  # save versioned output
+ln$Artifact$from_df(markers, key = "my-datasets/my-markers.parquet")$save()  # save versioned output
 ln$finish()  # finish the run, save source code & run report
