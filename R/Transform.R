67c4d3296b2873f702d95536c55b396d679f5d58
Luke Zappia <luke@data-intuitive.com>
2025-04-02 12:06:06 +0200

    ✨ Update view_lineage() to use returned graph

--- R/Transform.R
+++ R/Transform.R
@@ -3,8 +3,10 @@ py_to_r.lamindb.models.transform.Transform <- function(x) { # nolint object_leng
   wrap_python(
     x,
     public = list(
-      view_lineage = function(with_children = TRUE) {
-        view_lineage()
+      view_lineage = function(with_children = TRUE, return_graph = FALSE) {
+        view_lineage(
+          self, with_children = with_children, return_graph = return_graph
+        )
       }
     )
   )
