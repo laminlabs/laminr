dcdbe5315b40f559e3d093d8b03871eeddf7d437
Luke Zappia <lazappi@users.noreply.github.com>
2025-04-24 14:52:50 +0200

    🔖 Prepare v1.1.0 release (#193)
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>

--- R/laminr-package.R
+++ R/laminr-package.R
@@ -6,6 +6,7 @@
 #' @importFrom cli cli_abort cli_warn cli_inform
 #' @importFrom lifecycle deprecated
 #' @importFrom reticulate py_to_r r_to_py
-#' @importFrom utils .DollarNames
+#' @importFrom stats na.omit
+#' @importFrom utils .DollarNames head tail
 ## usethis namespace: end
 NULL
