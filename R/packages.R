224b77c3d05da0e5734532bdd6f8528cad7308ca
Luke Zappia <luke@data-intuitive.com>
2025-08-12 11:13:21 +0200

    ♻️ Replace packages helper functions

--- R/packages.R
+++ R/packages.R
@@ -1,34 +1,84 @@
-#' Get loaded packages
+#' Get R environment
 #'
-#' Get the currently loaded packages
+#' Get the current R environment to save when finishing a tracking run
 #'
-#' @returns A vector of the currently loaded package namespaces
+#' @returns A vector with package strings
 #' @noRd
-get_loaded_packages <- function() {
-  setdiff(loadedNamespaces(), "base")
+get_r_environment <- function() {
+  pkg_info <- sessioninfo::package_info(include_base = TRUE)
+
+  pkgs_df <- data.frame(
+    package = pkg_info$package,
+    version = pkg_info$loadedversion,
+    repo = get_package_sources(pkg_info$package)
+  )
+
+  paste0(pkgs_df$package, "==", pkgs_df$version, " @ ", pkgs_df$repo)
 }
 
-#' Get package repositories
+#' Get package sources
 #'
-#' Get source repositories associated with packages (excluding CRAN and
-#' Bioconductor)
+#' Get sources for a set of packages. This is similar to what is returned by
+#' `sessioninfo::package_info()` not truncated (see
+#' https://github.com/r-lib/sessioninfo/issues/116)
 #'
-#' @param packages A vector of packages to get repositories for
-#'
-#' @returns A vector of source repositories
+#' @param packages Vector of package names to get sources for
 #' @noRd
-get_package_repositories <- function(packages) {
-  repos <- purrr::map_chr(packages, \(.pkg) {
-    repo <- utils::packageDescription(.pkg)$Repository
-    if (is.null(repo)) {
-      NA_character_
-    } else {
-      repo
+get_package_sources <- function(packages) {
+  purrr::map_chr(packages, purrr::possibly(\(.pkg) {
+    desc <- utils::packageDescription(.pkg)
+    if (!is.null(desc$Repository)) {
+      return(desc$Repository)
     }
-  })
 
-  repos[repos != "CRAN" & !startsWith(repos, "Bioconductor")] |>
-    na.omit() |>
-    unique() |>
-    sort()
+    if (!is.null(desc$Priority) && desc$Priority == "base") {
+      return("base")
+    }
+
+    if (!is.null(desc$biocViews)) {
+      if (!is.null(desc$RemoteRepos)) {
+        bioc_version <- sub(".*packages/(\\d+\\.\\d+).*", "\\1", desc$RemoteRepos)
+      } else {
+        bioc_version <- ""
+      }
+
+      if (grepl("AnnotationData", desc$biocViews)) {
+        bioc_type <- "AnnotationData"
+      } else {
+        bioc_type <- ""
+      }
+
+      return(trimws(paste("Bioconductor", bioc_version, bioc_type)))
+    }
+
+    if (!is.null(desc$RemoteType)) {
+      remote_source <- desc$RemoteType
+
+      if (!is.null(desc$RemoteUsername) || !is.null(desc$RemoteRepo) || !is.null(desc$RemoteSha)) {
+        remote_source <- paste0(remote_source, " (")
+        if (!is.null(desc$RemoteUsername)) {
+          remote_source <- paste0(remote_source, desc$RemoteUsername)
+        }
+        if (!is.null(desc$RemoteRepo)) {
+          if (!is.null(desc$RemoteUsername)) {
+            remote_source <- paste0(remote_source, "/")
+          }
+          remote_source <- paste0(remote_source, desc$RemoteRepo)
+        }
+        if (!is.null(desc$RemoteSha)) {
+          remote_source <- paste0(remote_source, "@", desc$RemoteSha)
+        }
+        remote_source <- paste0(remote_source, ")")
+      }
+
+      return(remote_source)
+    }
+
+    if (pkgload::is_dev_package(.pkg)) {
+      return("pkgload")
+    }
+
+    return(NA_character_)
+  }, otherwise = "Unknown")) |>
+    purrr::set_names(packages)
 }
