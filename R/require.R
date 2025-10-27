3a938507bfe1a60a081f73773f5aedda69b9a85a
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-27 13:27:22 +0100

    🚧 Run with lamindb 1.13.0

--- R/require.R
+++ R/require.R
@@ -124,7 +124,7 @@ require_lamindb <- function(silent = FALSE) {
     require_module(
       "lamindb",
       options = laminr_lamindb_options,
-      version = ">=1.2",
+      version = ">=1.2,<=1.13.0",
       python_version = ">=3.10,<3.14",
       silent = silent
     )
